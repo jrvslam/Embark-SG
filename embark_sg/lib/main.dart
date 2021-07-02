b94d94dcbe01f502a4df5f735ce493830dd5e831
jrvslam <jarveis.lamzx@gmail.com>
2021-07-02 18:31:07 +0800

    finish homepage and add navigation bar

--- embark_sg/lib/main.dart
+++ embark_sg/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +22,17 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primaryColor: Color.fromRGBO(53, 20, 49, 1),
+        accentColor: Color.fromRGBO(239, 93, 168, 1),
+        scaffoldBackgroundColor: Color.fromRGBO(243, 247, 255, 1),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Color.fromRGBO(218, 218, 218, 1)),
+            foregroundColor: MaterialStateProperty.all(Color.fromRGBO(74, 74, 74, 1))
+          )
+        ),
+
+        fontFamily: 'Nunito',
+
       ),
       home: MyHomePage(title: 'Home'),
     );
@@ -46,6 +58,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class QuestionnaireCardWidget extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -97,20 +110,13 @@ class QuestionnaireCardWidget extends StatelessWidget {
                 height: 20,
               ),
               Container(
-                decoration: BoxDecoration(
-                  borderRadius : BorderRadius.all(Radius.circular(10)),
-                  color : Color.fromRGBO(217, 217, 217, 1),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                child: Text('Get started', textAlign: TextAlign.center, style: TextStyle(
-                    color: Color.fromRGBO(74, 74, 74, 1),
-                    fontFamily: 'Nunito',
-                    fontSize: 16,
-                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1
-                ),),
-              ),
+                  child: ElevatedButton(
+                    child: Text('Get Started'),
+                    onPressed: () {
+                      print('Received Click: Get Started');
+                    },
+                  )
+              )
             ]
           )
       )]
@@ -119,9 +125,9 @@ class QuestionnaireCardWidget extends StatelessWidget {
 }
 
 class RoadmapCard extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Row(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -187,35 +193,201 @@ class RoadmapCard extends StatelessWidget {
                 height: 20,
               ),
               Container(
-                decoration: BoxDecoration(
-                  borderRadius : BorderRadius.all(Radius.circular(10)),
-                  color : Color.fromRGBO(217, 217, 217, 1),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                child: Text('Start Planning', textAlign: TextAlign.center, style: TextStyle(
-                    color: Color.fromRGBO(74, 74, 74, 1),
-                    fontFamily: 'Nunito',
-                    fontSize: 16,
-                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1
-                ),),
-              ),
+                child: ElevatedButton(
+                  child: Text('Start Planning'),
+                  onPressed: () {
+                    print('Received Click: Start Planning');
+                  },
+                )
+              )
             ],
           )
         )
       ]
     );
   }
-
 }
 
+class LearnAboutSingapore extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width - 20,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(20),
+            decoration: new BoxDecoration(
+                color: Color.fromRGBO(255, 255, 255, 1),
+                borderRadius: new BorderRadius.all(Radius.circular(10))
+            ),
 
+            child:
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Learn more about Singapore', textAlign: TextAlign.center, style: TextStyle(
+                          color: Color.fromRGBO(74, 74, 74, 1),
+                          fontFamily: 'Nunito',
+                          fontSize: 20,
+                          letterSpacing: 0,
+                          fontWeight: FontWeight.bold,
+                          height: 1
+                      ),)
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        height: 150,
+                        width: 150,
+                        padding: EdgeInsets.all(20),
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(75),
+                          image: DecorationImage(
+                            image: AssetImage('img/foodinsg.jpg'),
+                            fit: BoxFit.cover,
+                            colorFilter: ColorFilter.mode(Colors.black26, BlendMode.darken)
+                          )
+                        ),
+                        child: Center(
+                            child: Text('Culture & Food', textAlign: TextAlign.center, style: TextStyle(
+                            fontFamily: 'Nunito',
+                            fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(252, 221, 236, 1)
+                        ))
+                        )
+                      ),
+                      Container(
+                        height: 150,
+                        width: 150,
+                        padding: EdgeInsets.all(20),
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(75),
+                            image: DecorationImage(
+                                image: AssetImage('img/transport.jpg'),
+                                fit: BoxFit.cover,
+                                colorFilter: ColorFilter.mode(Colors.black26, BlendMode.darken)
+                            )
+                        ),
+                        child: Center(
+                            child: Text('Getting Around', textAlign: TextAlign.center, style: TextStyle(
+                                fontFamily: 'Nunito',
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(252, 221, 236, 1)
+                            ))
+                        )
+                      ),
+                    ],
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        height: 150,
+                        width: 150,
+                        padding: EdgeInsets.all(20),
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(75),
+                            image: DecorationImage(
+                                image: AssetImage('img/lawinsg.jpg'),
+                                fit: BoxFit.cover,
+                                colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken)
+                            )
+                        ),
+                        child: Center(
+                            child: Text('Safety & Laws', textAlign: TextAlign.center, style: TextStyle(
+                                fontFamily: 'Nunito',
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(252, 221, 236, 1)
+                            ))
+                        )
+                      ),
+                      Container(
+                        height: 150,
+                        width: 150,
+                        padding: EdgeInsets.all(20),
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(75),
+                            image: DecorationImage(
+                                image: AssetImage('img/healthcareinsg.jpeg'),
+                                fit: BoxFit.cover,
+                                colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken)
+                            )
+                        ),
+                        child: Center(
+                            child: Text('Covid-19', textAlign: TextAlign.center, style: TextStyle(
+                                fontFamily: 'Nunito',
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(252, 221, 236, 1)
+                            ))
+                        )
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      ElevatedButton(
+                          child: Text('Local Happenings'),
+                          onPressed: () {
+                            print('Click Received: Local Happenings');
+                          }
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      ElevatedButton(
+                          child: Text('View More'),
+                          onPressed: () {
+                            print('Click Received: View More');
+                          }
+                      )
+                    ],
+                  )
+                ],
+              )
+          )
+          ]
+    );
+  }
+}
 
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -226,16 +398,24 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+    // floatingActionButton: FloatingActionButton(
+    //   onPressed: _incrementCounter,
+    //   tooltip: 'Increment',
+    //   child: Icon(Icons.add),
+    // )
+    //
+    // Text(
+    //   'You have pushed the button this many times:',
+    // ),
+    // Text(
+    // '$_counter',
+    // style: Theme.of(context).textTheme.headline4,
+    // ),
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final PageController controller = PageController(initialPage: 0);
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -243,38 +423,131 @@ class _MyHomePageState extends State<MyHomePage> {
         centerTitle: true,
         title: Text(widget.title),
       ),
-      body: Column(
-        // Column is also a layout widget. It takes a list of children and
-        // arranges them vertically. By default, it sizes itself to fit its
-        // children horizontally, and tries to be as tall as its parent.
-        //
-        // Invoke "debug painting" (press "p" in the console, choose the
-        // "Toggle Debug Paint" action from the Flutter Inspector in Android
-        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-        // to see the wireframe for each widget.
-        //
-        // Column has various properties to control how it sizes itself and
-        // how it positions its children. Here we use mainAxisAlignment to
-        // center the children vertically; the main axis here is the vertical
-        // axis because Columns are vertical (the cross axis would be
-        // horizontal).
-        children: <Widget>[
-          QuestionnaireCardWidget(),
-          RoadmapCard(),
-          Text(
-            'You have pushed the button this many times:',
+      body: PageView(
+          scrollDirection: Axis.horizontal,
+          controller: controller,
+          children: <Widget> [
+            ListView(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    QuestionnaireCardWidget(),
+                    RoadmapCard(),
+                    LearnAboutSingapore(),
+                  ],
+                ),
+              ]
+            ),
+          Center(
+            child: Text('Page 2'),
+          )]
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color.fromRGBO(53, 20, 49, 1.0),
+        selectedItemColor: Color.fromRGBO(239, 93, 168, 1),
+        unselectedItemColor: Color.fromRGBO(255, 181, 219, 1),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home'
           ),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.headline4,
+          BottomNavigationBarItem(
+              icon: Icon(Icons.map_outlined),
+              label: 'Roadmap'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.location_history_rounded),
+              label: 'News'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings'
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+      PageView(
+      scrollDirection: Axis.horizontal,
+      controller: controller,
+      children: <Widget> [
+        Scaffold(
+          appBar: AppBar(
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            centerTitle: true,
+            title: Text('Roadmap'),
+          ),
+          body: Center(
+            child: Text('Body'),
+          )
+        ),
+      ]
+    );
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     // Here we take the value from the MyHomePage object that was created by
+    //     // the App.build method, and use it to set our appbar title.
+    //     centerTitle: true,
+    //     title: Text(widget.title),
+    //   ),
+    //   body: ListView(
+    //     children: <Widget>[
+    //       Column(
+    //         // Column is also a layout widget. It takes a list of children and
+    //         // arranges them vertically. By default, it sizes itself to fit its
+    //         // children horizontally, and tries to be as tall as its parent.
+    //         //
+    //         // Invoke "debug painting" (press "p" in the console, choose the
+    //         // "Toggle Debug Paint" action from the Flutter Inspector in Android
+    //         // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+    //         // to see the wireframe for each widget.
+    //         //
+    //         // Column has various properties to control how it sizes itself and
+    //         // how it positions its children. Here we use mainAxisAlignment to
+    //         // center the children vertically; the main axis here is the vertical
+    //         // axis because Columns are vertical (the cross axis would be
+    //         // horizontal).
+    //         children: <Widget>[
+    //           QuestionnaireCardWidget(),
+    //           RoadmapCard(),
+    //           LearnAboutSingapore(),
+    //         ],
+    //       ),
+    //     ]
+    //   ),
+    //   bottomNavigationBar: BottomNavigationBar(
+    //     currentIndex: _selectedIndex,
+    //     onTap: _onItemTapped,
+    //     type: BottomNavigationBarType.fixed,
+    //     backgroundColor: Color.fromRGBO(53, 20, 49, 1.0),
+    //     selectedItemColor: Color.fromRGBO(239, 93, 168, 1),
+    //     unselectedItemColor: Color.fromRGBO(255, 181, 219, 1),
+    //     items: const <BottomNavigationBarItem>[
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.home),
+    //         label: 'Home'
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.map_outlined),
+    //         label: 'Roadmap'
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.location_history_rounded),
+    //         label: 'News'
+    //       ),
+    //       BottomNavigationBarItem(
+    //         icon: Icon(Icons.settings),
+    //         label: 'Settings'
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
